4c66d1e2c3c457a168eb9dd482a7000cdad56738
tissadeking <34912974+tissadeking@users.noreply.github.com>
2021-01-25 09:31:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request
+#Importing the Libraries
+import numpy as np
+from flask import Flask, request,render_template
 from flask_cors import CORS
+import os
 import pickle
 import flask
 import os
 import newspaper
 from newspaper import Article
 import urllib
+
 
 app = Flask(__name__)
 CORS(app)
@@ -16,7 +20,7 @@ with open('model.pickle', 'rb') as handle:
 
 @app.route('/')
 def main():
-    return render_template('htmlfile.html')
+    return render_template('index.html')
 @app.route('/predict',methods = ['GET', 'POST'])
 def predict():
     url = request.get_data(as_text = True)[5:]
@@ -28,7 +32,7 @@ def predict():
     news = article.summary
 
     label_prediction = model.predict([news])
-    return render_template('htmlfile.html', prediction_text = 'Result: {}'.format(label_prediction[0]))
+    return render_template('index.html', prediction_text = 'Result: {}'.format(label_prediction[0]))
 
 if __name__ == "__main__":
     port = int(os.environ.get('port', 5000))
